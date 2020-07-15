7545657bb15540840aad6c9e511a362b7eb94e34
arbhassan <hassanjawad1321@gmail.com>
2020-07-16 00:58:52 +0500

    Passed and Read Params Upon Navigation

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
 
-const CategoriesMealsScreen = (props) => {
+const CategoriesMealsScreen = ({ route, navigation }) => {
+  const catId = route.params.categoryId;
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
   return (
     <View style={styles.screen}>
       <Text>The Categores Meals Screen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Details!"
         onPress={() => {
-          props.navigation.navigate("MealDetail");
+          navigation.navigate("MealDetail");
         }}
       />
       <Button
         title="Go Back"
         onPress={() => {
-          props.navigation.pop();
+          navigation.pop();
         }}
       />
     </View>
